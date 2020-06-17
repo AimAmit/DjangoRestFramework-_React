ba725ca19290264a5fd0a2e04a7455bbcd31df64
Amit <contacttoamit00@gmail.com>
2020-06-17 17:32:27 +0530

    fixed axios auth

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
-let header = sessionStorage.getItem('token') ?
-    { Authorization: `Token ${sessionStorage.getItem('token')}` } : null
-
 let instance = axios.create({
     baseURL: '/',
-    headers: header
 })
 
+instance.interceptors.request.use(config => {
+    const token = sessionStorage.getItem('token')
+
+    config.headers.Authorization = token ? `Token ${token}` : ''
+    return config
+})
 
 export default instance
